3a0a1d8b1aa442706570a964db7042d2355f20f7
satovarmo <satovarmo@unal.edu.co>
2022-11-30 20:30:12 -0500

    Comentarios program y lexer

--- BasicLexer.py
+++ BasicLexer.py
@@ -2,33 +2,42 @@ import ply.lex as lex
 from utils import TOKENS, RESERVED
 
 class basic_lexer():
+    #Obtenemos los tokens a partir del archivo utils donde fueron definidos
     tokens = TOKENS
 
-    t_NUMBER = r'[0-9]+\s'
-    t_INT = '[0-9]+'
-    t_OP = r'\+|-'
-    t_ASSIGN = r'='
-    t_SEP = r'\,'
+    #Específicamos las regular expressions para la mayoría de los tokens
+    t_NUMBER = r'[0-9]+\s'          #Representa la numeración de cada línea de código, por eso es un entero seguido de un espacio
+    t_INT = '[0-9]+'                #Un numero entero
+    t_OP = r'\+|-'                  #Operaciones aritméticas válidas
+    t_ASSIGN = r'='                 #Simbolo de igualadad para la asignación
+    t_SEP = r'\,'                   #Símbolo de coma para la separación de variables
 
+
+    #Definimos el token STRING mediante una función para garantizar que la STRING ingresada no haga parte de las palabras reservadas
     def t_STRING(self,t):
-        r'"([a-z|A-Z|0-9]+\s*[a-z|A-Z|0-9]*)+"'
-        reserved=RESERVED
-        t.type = reserved.get(t.value,'STRING')    # Check for reserved words
+        r'"([a-z|A-Z|0-9]+\s*[a-z|A-Z|0-9]*)+"'     #Definimos en primera instancia el token mediante su regular expression
+                                                    #Al menos un carácter, seguido de un posible espacio con más carácteres. Todo esto al menos una vez
+        reserved=RESERVED                           #Llamamos a las palabras reservadas
+        t.type = reserved.get(t.value,'STRING')     #Revisamos que STRING no esté en ellas
         return t
 
-
+    #Definimos el token VAR mediante una función para garantizar que la VAR ingresada no haga parte de las palabras reservadas
     def t_VAR(self,t):
-        r'[A-Z]+'
-        reserved = RESERVED
-        t.type = reserved.get(t.value, 'VAR')  # Check for reserved words
+        r'[A-Z]+'                               #Definimos el token con su regular expresion
+                                                #Al menos una letra en mayúscula 
+        reserved = RESERVED                     #Llamamos a las palabras reservadas
+        t.type = reserved.get(t.value, 'VAR')   #Revisamos que VAR no esté en ellas
         return t
 
+    #Ingresamos los tokens que ignoraremos, en este caso tab
     t_ignore = ' \t'
 
+    #La función error identifica cuando un carácter no coincide con ninguno de los tokens propuestos e imprime el error
     def t_error(self,t):
         print("Illegal character '%s'" % t.value[0])
         t.lexer.skip(1)
 
+    #Testea un input y extrae los tokens de la misma
     def test(self, data):
         self.lexer.input(data)
         while True:
@@ -37,6 +46,8 @@ class basic_lexer():
                 break
             print(tok)
 
+
+    
     def __init__(self):
         self.lexer = lex.lex(module=self)
 
--- Program.py
+++ Program.py
@@ -2,28 +2,40 @@ from BasicLexer import basic_lexer
 from BasicParser import basic_parser
 import re
 
+#Clase programa que ejecutará el ingreso de las lineas en BASIC
 class program:
+    #Constructor para la clase programa
     def __init__(self):
-        self.lineas = list("")
-        self.mylexer = basic_lexer()
-        self.myparser = basic_parser()
+        self.lineas = list("")              #Atributo para guardar las lineas de codigo ingresadas
+        self.mylexer = basic_lexer()        #Atributo para crear el Lexer 
+        self.myparser = basic_parser()      #Atributo para crear el parser
 
+
+    #Metodo de ejecución para el programa
     def execute(self):
-        line=input()
-        while(line!="RUN"):
-
+        line=input()                            #El usuario ingresa una linea de código
+        while(line!="RUN"):                     #Mientras que el usuario no ingrese "RUN" y no haya errores, permitirá ingresar más
             result = self.agregar_lineas(line)
             if result == "error":
                 break
             line=input()
         i=0
+        #Recorremos cada una de las líneas de código en el atributo lineas, omitiendo las vacías
         while (i<len(self.lineas)):
             e=self.lineas[i]
             if(e!=''):
-                result = self.myparser.parser.parse(input=e, lexer=self.mylexer.lexer)
+                result = self.myparser.parser.parse(input=e, lexer=self.mylexer.lexer)          #Ejecutamos el parser para cada linea no vacía
                 if(self.myparser.goto_index!=-1):
+                    #Si la linea es un comando GOTO, el goto_index ya no será -1, así que ingresamos al if y evaluamos si el índice
+                        #de la nueva línea de código es válido, es decir nos re dirige a una línea existente y no vacía
+                    if(self.myparser.goto_index>=len(self.lineas) or self.lineas[self.myparser.goto_index]==''):
+                        #Si el índice no es válido, no imprime el error e interrumpe la ejecución del código
+                        print(f"error in line {i}")        
+                        break
+                    #Si el índice es válido, se reasigna al contador que recorre el arreglo lineas y se reestable el goto_index
                     i=self.myparser.goto_index-1
                     self.myparser.goto_index=-1
+                #Si el parser retorna un error, se interrumpe la ejecución    
                 if result:
                     if ("error" in result):
                         print(result)
@@ -32,16 +44,16 @@ class program:
 
 
 
-
+    #El metodo agregar lineas nos permite insertar una nueva línea de código en BASIC al arreglo lineas
     def agregar_lineas(self,line):
+        #Primero extraemos la numeración de la linea recién escrita
         list_found_num = re.findall('[0-9]+\s', line)
         if len(list_found_num) > 0:
             num = int(re.findall('[0-9]+\s', line)[0])
-
-            if (num < len(self.lineas)):
+            if (num < len(self.lineas)):                    #Si la numeración es menor a la long. de la lista, entonces reemplazamos lo que esté en esa posición del arreglo
                 self.lineas[num] = line
             else:
-                for i in range(len(self.lineas), num):
+                for i in range(len(self.lineas), num):     #Si la numeración es mayor, entonces añadimos elementos vacíos a la lista hasta llegar al índice deseado
                     self.lineas.append("")
                 self.lineas.append(line)
         else:
@@ -49,5 +61,6 @@ class program:
             return "error"
 
 
+#Creamos un programa, y lo ejecutamos
 p=program()
 p.execute()
